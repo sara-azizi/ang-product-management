bd6c157c02e4c1f0e76b60385fe545a6afa928f4
sara-azizi <azizisara220@gmail.com>
2025-03-16 00:18:21 +0330

    bug fixed

--- src/app/date-input/date-input.component.ts
+++ src/app/date-input/date-input.component.ts
@@ -8,13 +8,13 @@ import { invalid } from 'jalali-moment';
   styleUrls: ['./date-input.component.css'],
   standalone: false
 })
-export class DateInputComponent{
+
+export class DateInputComponent {
   @Output() formSubmitted = new EventEmitter<string>();
 
   dateForm: FormGroup;
+  textError: string = '';
 
-  textError: string = ''
-  
   constructor(private fb: FormBuilder) {
     this.dateForm = this.fb.group({
       persianDate: ['', [
@@ -23,11 +23,15 @@ export class DateInputComponent{
         this.persianDateValidator()
       ]]
     }, { updateOn: 'change' });
+
+    this.dateForm.get('persianDate')?.valueChanges.subscribe(() => {
+      this.textError = this.getTextError();
+    });
   }
 
   persianDateValidator(): ValidatorFn {
     return (control: any) => {
-      if (!control.value) return {invalidValue: 'تاریخ الزامی است'};
+      if (!control.value) return { required: 'تاریخ الزامی است' };
 
       const parts = control.value.split('/');
       if (parts.length !== 3) return { invalidFormat: 'فرمت تاریخ باید به صورت yyyy/mm/dd باشد' };
@@ -37,9 +41,9 @@ export class DateInputComponent{
       const day = parseInt(parts[2], 10);
 
       if (year < 1300 || year > 1500) return { invalidYear: 'سال باید بین 1300 و 1500 باشد' };
-      if (month < 1 || month > 12) return { invalidMonth: 'ماه نامعبتر است' };
+      if (month < 1 || month > 12) return { invalidMonth: 'ماه نامعتبر است' };
       const daysInMonth = month <= 6 ? 31 : month <= 11 ? 30 : 29;
-      if (day < 1 || day > daysInMonth) return { invalidDay: `روز باید بین ۱ تا ${daysInMonth} باشد`  };
+      if (day < 1 || day > daysInMonth) return { invalidDay: `روز باید بین ۱ تا ${daysInMonth} باشد` };
 
       return null;
     };
@@ -50,6 +54,7 @@ export class DateInputComponent{
   }
 
   onSubmit() {
+    this.textError = this.getTextError();
     if (this.dateForm.valid) {
       this.formSubmitted.emit(this.dateForm.value.persianDate);
       console.log('تاریخ درست است:', this.dateForm.value.persianDate);
@@ -59,28 +64,15 @@ export class DateInputComponent{
     }
   }
 
-  // public getTextEror(): string {
-  //   console.log(this.dateForm);
-    
-    
-  //   if (this.dateForm.get('persianDate')?.errors){
-  //     return this.dateForm.get()
-  //   }
-  //   return ''
-  // }
-
-  
   public getTextError(): string {
     const errors = this.dateForm.get('persianDate')?.errors;
-    if (!errors) {
-      return ''; 
-    }
+    if (!errors) return '';
     return (
-      (errors['invalidFormat'] as string) ||
-      (errors['invalidYear'] as string) ||
-      (errors['invalidMonth'] as string) ||
-      (errors['invalidDay'] as string) ||
-      (errors['required'] as string) ||
+      errors['required'] ||
+      errors['invalidFormat'] ||
+      errors['invalidYear'] ||
+      errors['invalidMonth'] ||
+      errors['invalidDay'] ||
       ''
     );
   }
